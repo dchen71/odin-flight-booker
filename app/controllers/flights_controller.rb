ce2b6756d36d78ef5374dd9a7b0316a732403e07
Dan <dchen12812@gmail.com>
2015-06-29 18:34:30 -0700

    implemented search and resulting time page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,9 @@
 class FlightsController < ApplicationController
 	def index
 		@flight_options = Flight.all.map{|f| [f.start_date]}.to_set #map dates to array then to set to make unique values
-		@airport_options = Airport.all.map{|a| [a.code, a.city]} #code, value=city
+		@airport_options = Airport.all.map{|a| [a.code, a.id]}
 		@passengers = (1..4).map{|p| [p.to_s]}
+
+		@flights = Flight.search(params)
 	end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,13 @@
 class Flight < ActiveRecord::Base
 	belongs_to :to_airport, class_name: "Airport"
 	belongs_to :from_airport, class_name: "Airport"
+
+	def self.search(params)
+		if params[:search] #ensure params are from search
+			Flight.where(from_airport_id: params[:depart_code], to_airport_id: params[:arrive_code], 
+				start_date: params[:start_date])
+		else
+			Flight.none
+		end
+	end
 end
